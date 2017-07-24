ef89ca08627fe5e9373bd4cc0688091784152a07
ilhamsabir <ilhamsabir3@gmail.com>
2017-07-24 13:38:37 +0700

    change mongoDb config

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -3,13 +3,17 @@
 var mongoose = require('mongoose');
 var Product = mongoose.model('Products');
 
-// export retur data
+// export return data
 exports.list_all_product = function(req, res) {
   Product.find({}, function(err, data) {
     if (err) {
         res.send(err);
       }
       // res.json(data);
-      res.json({'data': data});
+      res.json({
+          'data': data,
+          'message': 'success',
+          'error': 0,
+        });
   });
 };
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var bodyParser = require('body-parser');
 var Product    = require('./app/models/product');
 
 // Mongo DB Connet
-mongoose.connect('mongodb://localhost:27017/newsdata');
+// mongoose.connect('mongodb://localhost:27017/newsdata');
+mongoose.connect('mongodb://ilhamsabir:Illank1991@ds041150.mlab.com:41150/sampledata_mlab');
 
 // express
 var app = express();
